f84702d7a4932ce3e547ab41c28e5b088d6748dc
Bogdan Lab <vba@rams.colostate.edu>
2020-02-23 14:50:07 -0700

    test page

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html>
     
@@ -12,26 +14,36 @@
         <h1> Welcome and let's try to do some stuff</h1>
         <p>Let's say we need to make a user input something in a <i>field</i> and another label will show a result of what they entered.</p>
         
-        
-        <form>
+        <?php
+	 $a = " ";
+	 $b = " ";
+	 $sum = " ";?>
+        <form name="form" action="" method="get">
             <label id="num1">Enter a number here</label><br>
             <!--- The field below has ID num1 -->
-            <input type="number" id="num1" name="number1"><br>
+            <input type="number" name="num1"><br>
             
             <!--- The field below has ID num2 -->
             <label for="num2">Enter a number here</label><br>
-            <input type="number" id="num2" name="number2">
+            <input type="number"  name="num2">
+	    <input type="submit" name="submit" value="Submit">
         </form>
-            <button type="button" id="btn"> Hit this to compute!</button>
+            
 
         <!-- possible space for computation -->
         <?php 
-        echo("He!");
+        
+	 if(isset($_GET['num1'])){    
+	    $a = $_GET['num1'];
+	    $b = $_GET['num2'];
+	    $sum = $a + $b;
+	    
+	    }
         ?>
-        <h1>The sum of [replace with num1] and [replace with num2] is </h1>
-        <h2>[Answer]!</h2>
+        <h1>The sum of <?php echo $a;?> and <?php echo $b;?> is </h1>
+        <h2><?php if($sum != 0) { echo "Sum is ", $sum;} ?></h2>
         
-        
+        <?php session_destroy(); ?>
         
     </body>
 </html>
